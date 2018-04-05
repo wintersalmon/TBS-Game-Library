7fc27a7cf4093aef0eb5a8455e04ffcb726ccb58
김형준 <wintersalmon@gimhyeongjun-ui-MacBook-Pro.local>
2018-04-05 16:13:01 +0900

    Merged dev-cli-improvement, minor improvements and documents added

---
+++ documents/layer.md
@@ -0,0 +1,19 @@
+## Class Layer
+
+### Tier 1
+- Game, Events
+- logic and data layer
+- handles game logic
+- Game represents DATA, Event represents LOGIC
+
+
+### Tier 2
+- Wrapper, Manager
+- File Management layer
+- handles GameFile(Create,Load,Save), GameUpdate, Replay Managements
+
+
+### Tier 3
+- UI
+- handles User Interface
+- Top layer of the system
--- src/core/ui.py
+++ src/core/ui.py
@@ -66,6 +66,7 @@ class CLIPlay(CLI):
         while self._main_loop():
             self.draw()
 
+<<<<<<< HEAD
     def _create_game(self):
         raise NotImplementedError
 
@@ -75,6 +76,8 @@ class CLIPlay(CLI):
     def draw(self):
         print(self.manager)
 
+=======
+>>>>>>> dev-cli-improvement
     def save(self, *, save_as=None):
         if save_as:
             save_file_name = save_as
@@ -91,3 +94,12 @@ class CLIPlay(CLI):
 
         if save_file_name:
             self.manager.save(self.save_dir, save_file_name)
+<<<<<<< HEAD
+=======
+
+    def _create_game(self):
+        raise NotImplementedError
+
+    def _main_loop(self):
+        raise NotImplementedError
+>>>>>>> dev-cli-improvement
